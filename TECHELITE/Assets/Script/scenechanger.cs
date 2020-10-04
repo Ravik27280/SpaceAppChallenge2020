9fa09e5fc6d0b850a71a551e10457e96c6a5706a
Ravik27280 <Ravik27280@gmaill.com>
2020-10-04 11:50:00 +0530

    update

---
+++ TECHELITE/Assets/Script/scenechanger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scenechanger : MonoBehaviour
+{
+   public void mainmanu()
+    {
+        SceneManager.LoadScene("mainmanu");
+    }
+    public void SpaceShip()
+    {
+        SceneManager.LoadScene("SpaceShip");
+    }
+    public void moon()
+    {
+        SceneManager.LoadScene("moon");
+    }
+    //public void scene4()
+    //{
+    //    SceneManager.LoadScene("menu");
+    //}
+}
